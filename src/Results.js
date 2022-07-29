537dbe92911864da2d0e6163b651004835daf93a
KimiaMontazeri <kimia.montazeri@yahoo.com>
2022-07-29 17:15:50 +0430

    Add some css to Pet and Results components

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,7 @@ import Pet from "./Pet";
 // ternaries are a lot used in react
 const Results = ({ pets }) => {
   return (
-    <div>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {!pets.length ? (
         <h1>No pets found!</h1>
       ) : (
